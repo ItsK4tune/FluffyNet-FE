0e57d59db45929e6cbbc4222460460bf80c137a6
ItsK4tune <duongcuteyb@gmail.com>
2025-03-26 12:11:03 +0700

    Setup template

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from "react";
+// import { Outlet } from "react-router-dom";
+
+function App() {
+	// const [isLoading, setIsLoading] = useState(true);
+	// useEffect(() => {}, []);
+
+	return (
+		<>
+			{/* <Header /> */}
+			{/* {!isLoading && <Outlet />} */}
+		</>
+	);
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./index.css";
-
-import { Button } from './components/element/button'
-import { Input } from './components/element/input'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Tabs defaultValue="tab1">
-      <TabsList>
-        <TabsTrigger value="tab1">Tab 1</TabsTrigger>
-        <TabsTrigger value="tab2">Tab 2</TabsTrigger>
-      </TabsList>
-        <TabsContent value="tab1">Body of Tab 1</TabsContent>
-        <TabsContent value="tab2">Body of Tab 2</TabsContent>
-    </Tabs>
-    <Input/>
-    <Button/>
-  </StrictMode>,
+  </StrictMode>
 )
